a3ae8ac5bcf8ab790455f1e6916e3cf55638968b
Thulashika <lasibala24@gmail.com>
2024-12-26 14:13:01 +0530

    Created a stateless widget called HelloWorldApp and added AppBar.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
 
-// add two numbers and print
-void addTwoNumbers(double numberOne, double numberTwo) {
-  // int numberOne = 5;
-  // int numberTwo = 10;
-
-  //add two numbers
-  double sum = numberOne + numberTwo;
-
-  //print the sum
-  print(sum);
-}
-
-void divideTwoNumbers(double numberOne, double numberTwo) {
-
-  double divide = numberOne/numberTwo;
-
-  print(divide);
-}
-
 void main() {
-  // runApp();
-  addTwoNumbers(7.3, 76.87);
-  divideTwoNumbers(10.0, 5.0);
+  runApp(MaterialApp(
+    home: HelloWorldApp(),
+  ));
 }
 
-//task 01
-/*
-write a function that divides a number with anotherr number and prints the result.
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({super.key});
 
-variables should use the lowerCamelCase naming convention ---> numberOne
-functions should also use the lowerCamelCase naming convention ---> addTwoNumbers
-*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is the App Bar!'),
+        backgroundColor: Colors.blue.shade400,
+      ),
+    );
+  }
+}
