e49dbcd7fa27d261e46fdb5defff2c18e185735f
Thulashika <lasibala24@gmail.com>
2024-12-27 22:01:23 +0530

    dice roll app almost complete!.

--- dice_roll_application/lib/main.dart
+++ dice_roll_application/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -6,8 +9,16 @@ void main() {
   ));
 }
 
-class DiceRollApp extends StatelessWidget {
+class DiceRollApp extends StatefulWidget {
   const DiceRollApp({super.key});
+
+  @override
+  State<DiceRollApp> createState() => _DiceRollAppState();
+}
+
+class _DiceRollAppState extends State<DiceRollApp> {
+
+  String path = 'assets/images/dice-1.png';
 
   @override
   Widget build(BuildContext context) {
@@ -27,25 +38,38 @@ class DiceRollApp extends StatelessWidget {
                 color: Colors.black.withOpacity(0.41)),
             ),
             Image.asset(
-              'assets/images/dice-1.png', 
+              path, 
               height: 225, 
               width: 225,
             ),
-            Container(
-              height: 51,
-              width: MediaQuery.of(context).size.width * 0.7,
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 29, 46, 29),
-                borderRadius: BorderRadius.circular(12.0)),
-              child: Center(
-                child: Text(
-                  'ROLL DICE',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-              )),
+            GestureDetector(
+              onTap: printRanNum,
+              child: Container(
+                height: 51,
+                width: MediaQuery.of(context).size.width * 0.7,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 29, 46, 29),
+                  borderRadius: BorderRadius.circular(12.0)),
+                child: Center(
+                  child: Text(
+                    'ROLL DICE',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                )),
+              ),
             )
           ],
         ),
       ),
     );
   } 
+
+  void printRanNum() {
+    int randNumber = Random().nextInt(6) + 1;
+    print('the image path is $path');
+    
+    setState(() {
+      path = 'assets/images/dice-$randNumber.png';
+    });
+    print('The new image path is $path');
+  }
 } 
